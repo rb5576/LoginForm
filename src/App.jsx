d4f8c6977dff2f016c728b1399654b2fe4c0d86e
Rahul kumar <rk094988@gmail.com>
2024-11-07 13:01:30 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+import LoginForm from './LoginForm'
+
+function App() {
+
+  return (
+    <>
+    <LoginForm/>
+    </>
+  )
+}
+
+export default App;
